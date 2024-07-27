8a87edf798b475ec6f71e6e7404eab77f7b8adb3
Daniel Ceregatti <Daniel.Ceregatti@PolicyMap.com>
2024-07-26 21:44:21 -0700

    Refactor docker compose build so a single server image is built for however many servers. Add horse mod, despite it being known to crash the Linux server. One day... Fix bashrc getting appended to instead of written to. Oops!

---
+++ files/mods/3295021220/start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# This is run in the directory with the target files
+
+# Copy the territory file over
+cp ${FILES}/mods/${ID}/horses_chernarus.xml env/wild_horse_territories.xml
